b5cd66a4e54a1833dbb00e7168d6f2b4b1dddd4d
DimaPasyutin <dima.pasyutin2@gmail.com>
2021-02-07 13:56:17 +0200

    Добавил коментарии(разобрал код) и добавил смахивание основного DrawerLayout при открии меню

--- app/src/main/java/com/example/menutest/MainActivity.java
+++ app/src/main/java/com/example/menutest/MainActivity.java
@@ -8,6 +8,8 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -24,20 +26,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final ConstraintLayout content = findViewById(R.id.content);
+
+        // Инициализируем и устанвливаем бар (верхняя монолитная иконка)
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // Инициализируем DrawerLayout (улучшеная верся FrameLayout)
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.icon_timetable, R.id.icon_navigation, R.id.nav_slideshow)
+                R.id.icon_timetable, R.id.icon_navigation)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+//      Отвечает за смахивание содержимого при открывании меню
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string
+                .navigation_drawer_close) {
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+                super.onDrawerSlide(drawerView, slideOffset);
+
+                float slideX = drawerView.getWidth() * slideOffset;
+                content.setTranslationX(slideX);
+
+                // а также меняем размер
+                content.setScaleX(1 - slideOffset);
+                content.setScaleY(1 - slideOffset);
+            }
+        };
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
     }
 
     @Override
@@ -53,4 +79,5 @@ public class MainActivity extends AppCompatActivity {
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
+
 }
